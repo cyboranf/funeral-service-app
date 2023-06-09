ec98ed470ef6d058c709bd35399c6021d71763e8
cyboranf <cyboranf@gmail.com>
2023-06-09 17:32:57 +0200

    config -changed

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,17 @@ use App\Http\Controllers\CoffinController;
 use App\Http\Controllers\FuneralController;
 use App\Http\Controllers\PriestController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
 
 
 
@@ -21,22 +28,21 @@ Route::resource('users', UserController::class);
 
 
 // Routing dla logowania
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('loginPost');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 // Routing dla rejestracji
-Route::get('register', [UserController::class, 'create'])->name('register');
-Route::post('register', [UserController::class, 'store']);
+Route::view('register', 'auth.register')->name('register');
+Route::post('register', [UserController::class, 'register']);
+
+
 
 // Routing dla zarządzania użytkownikami
 
-    Route::get('users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
     Route::get('users/{user}', [UserController::class, 'show'])->name('users.show');
     Route::get('users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
     Route::put('users/{user}', [UserController::class, 'update'])->name('users.update');
     Route::delete('users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
-    Route::get('/about', function () {
-        return view('about');
-    })->name('about');
